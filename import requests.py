ccf9d0b5183d86f9a4cce92593824332dd450a87
Alexander Rodriguez <30503623+rodri270@users.noreply.github.com>
2024-02-15 13:49:42 -0500

    initial file upload to the repo

---
+++ import requests.py
@@ -0,0 +1,78 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import pprint
+import sys
+import json
+import pandas as pd
+import csv
+import base64, email.utils, hmac, hashlib, urllib
+import duo_client
+from six.moves import input
+
+admin_api = duo_client.Admin(
+    ikey='*****',
+    skey='*******',
+    host='api-302859b9.duosecurity.com',
+    ca_certs='DISABLE'
+)
+
+response = admin_api.json_api_call(
+    'GET',
+    '/admin/v1/bypass_codes',
+    {
+        'limit':'500'
+    }
+)
+
+pprint.pprint(response)
+
+csv_filename='responses.csv'
+bypass_code_ids={}
+
+bypasslist=[]
+with open(csv_filename, mode='w', newline="") as file:
+    fieldnames = ['firstname', 'lastname', 'email', 'username', 'bypass_code_id','fullname']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for resp in response:
+        row = {
+            
+            'firstname': resp['user']['firstname'],
+            'lastname': resp['user']['lastname'],
+            'email': resp['user']['email'],
+            'username': resp['user']['username'],
+            'fullname': resp['user']['realname'],
+            'bypass_code_id': resp['bypass_code_id']
+        }
+        writer.writerow(row)
+        bypass_code_id = resp['bypass_code_id']
+        full_name = resp['user']['realname']
+        bypass_code_ids[full_name]= bypass_code_id
+        
+        bypasslist.append(resp['bypass_code_id'])
+        
+print(f"CSV file '{csv_filename}' has been created successfully.")
+'''
+bypass_removal=[]
+for code in bypasslist:
+    bypass_removal.append(admin_api.json_api_call(
+        'DELETE',
+        f'/admin/v1/bypass_codes/{code}',
+        {}
+        ))    
+    pprint.pprint(bypass_removal)
+
+
+
+print(bypass_code_ids)
+'''
+
+
+create_code = admin_api.json_api_call(
+    'POST',
+    '/admin/v1/users/[user_id]/bypass_codes'
+                                    
+                          )
+
